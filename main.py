58b17b8397aa0dd6bd5b2088b1430e4e495e0bdd
Prathik M <murthy.prathik@gmail.com>
2021-11-08 23:54:00 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,96 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import requests
+import json
+
+def lastfm_get(payload):
+    # define headers and URL
+    headers = {'user-agent': 'prathikm'}
+    url = 'https://ws.audioscrobbler.com/2.0/'
+
+    # Add API key and format to the payload
+    payload['api_key'] = '8e2070bb8aa8fbb8bd8f934fdcc1b2d1'
+    payload['format'] = 'json'
+
+    response = requests.get(url, headers=headers, params=payload)
+    return response
+
+def jprint(obj):
+    # create a formatted string of the Python JSON object
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+scope = "playlist-modify-public"
+user='pmurthy20'
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='0040783bc8f143ac8be71702bab0903f', client_secret='fed0d04254864b6e969a96849e851dd5', scope=scope, redirect_uri='http://example.com'))
+
+
+
+# results = sp.current_user_saved_tracks()
+# for idx, item in enumerate(results['items']):
+#     track = item['track']
+#     print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+# sp.user_playlist_create(user='pmurthy20', name='test', public=True, description='test')
+
+# sp.user_playlist_create(user='pmurthy20', name='last.fm Liked Songs', public=True, description='This playlist is automatically updated using a Python script whenever a new song is liked on last.fm :)')
+
+
+results=sp.user_playlists(user=user)
+
+playlist_list = []
+for idx, item in enumerate(results['items']):
+    # print(item['name'])
+    if item['name'] == 'last.fm Liked Songs':
+        playlist_list.append((item['name'], item['id']))
+
+# if len(playlist_list) != 0:
+#     print(playlist_list)
+
+# Needs to be reimplemented at some point
+# if 'test' not in playlist_list:
+
+
+
+
+
+r = lastfm_get({
+    'method': 'user.getLovedTracks',
+    'user': 'prathikm'
+})
+
+# jprint(r.json())
+
+loved_tracks = []
+for idx, item in enumerate(r.json()['lovedtracks']['track']):
+    track_name = item['name']
+    track_artist = item['artist']['name']
+    loved_tracks.append((track_name, track_artist))
+
+# print(loved_tracks)
+
+spotify_playlist_items = sp.playlist_items(playlist_id=playlist_list[0][1])['items']
+spotify_playlist_names = []
+for track in spotify_playlist_items:
+    jprint(track['track']['name'])
+    spotify_playlist_names.append(track['track']['name'])
+
+# jprint(spotify_playlist_names)
+
+
+# for x in range(len(loved_tracks)):
+
+# print('track:'+loved_tracks[x][0]+' artist:'+loved_tracks[x][1])
+for x in loved_tracks:
+    results= sp.search('track:'+str(x[0])+' artist:'+str(x[1]), type='track', limit=1)
+    if len(results['tracks']['items']) > 0:
+        if results['tracks']['items'][0]['name'] not in spotify_playlist_names:
+            sp.playlist_add_items(playlist_id=playlist_list[0][1], items=[results['tracks']['items'][0]['uri'][14:]])
+
+        # sp.playlist_remove_all_occurrences_of_items(playlist_id=playlist_list[0][1], items=[results['tracks']['items'][0]['uri'][14:]])
+    else:
+        print('Unable to add to Playlist: ' + 'track:'+str(x[0]) + ' last.fm artist: ' +str(x[1]))
+        
+    
+
